53cb5b8ee3b2f7eb78f90a59490f6f26983b9df9
cliff <cliff.wong@ehlacademy.org>
2022-12-06 15:15:11 +0800

    use aws db

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', 'localhost'),
-      port: env.int('DATABASE_PORT', 8889),
+      host: env('DATABASE_HOST', 'strapi-database.ckjfdmyszhad.ap-southeast-1.rds.amazonaws.com'),
+      port: env.int('DATABASE_PORT', 3306),
       database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', 'root'),
+      user: env('DATABASE_USERNAME', 'cliffwong'),
+      password: env('DATABASE_PASSWORD', 'Yy27022070'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
