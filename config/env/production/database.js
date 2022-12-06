1f70c5c323a154a59989cadb645d6efad704cce6
cliff <cliff.wong@ehlacademy.org>
2022-12-06 15:57:04 +0800

    update dtabase config

---
+++ config/env/production/database.js
@@ -0,0 +1,16 @@
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', 'strapi-database.ckjfdmyszhad.ap-southeast-1.rds.amazonaws.com'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'cliffwong'),
+      password: env('DATABASE_PASSWORD', 'Yy27022070'),
+      ssl: {
+        rejectUnauthorized: false
+      },
+    },
+    debug: false,
+  },
+});
