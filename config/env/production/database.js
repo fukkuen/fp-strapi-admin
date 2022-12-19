175f7b937b6aa96700ddabf53e7a7157397f2d20
cliff <cliff.wong@ehlacademy.org>
2022-12-19 15:45:59 +0800

    deploy to ec2 instead of heroku, testing

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -5,14 +5,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: {
-        rejectUnauthorized: false
-      },
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "Yy27022070"),
     },
     debug: false,
   },
