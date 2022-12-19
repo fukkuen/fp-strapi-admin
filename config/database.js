e6b794ea0ac13ae6a7fe2cb09a41add71401956a
cliff <cliff.wong@ehlacademy.org>
2022-12-19 17:54:16 +0800

    hardcode data credential 2

--- config/database.js
+++ config/database.js
@@ -1,13 +1,14 @@
+console.log('loading dev database.js')
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'localhost'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'cliff'),
-      user: env('DATABASE_USERNAME', 'cliff'),
-      password: env('DATABASE_PASSWORD', ''),
-      ssl: env.bool('DATABASE_SSL', false)
-    }
+      host: "strapi-database.cejgpwrykkoc.ap-southeast-1.rds.amazonaws.com",
+      port: 5432,
+      database: "strapi",
+      user: "postgres",
+      password: "Yy27022070",
+    },
+    debug: false,
   },
 });
